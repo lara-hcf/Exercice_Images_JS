1d8113df438fb3477f0764a5d28f2811ab73415a
lara-hcf <lara.hocf@gmail.com>
2024-04-10 16:20:56 +0200

    update checkbox hidden and visible

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
 function cocherImage() {
     if (document.getElementById('coche1').checked) {
-        document.getElementById('image1').style.display="none";
+        document.getElementById('image1').style.display="block";
     } else {
-        document.getElementById("image1").style.display="block";
+        document.getElementById("image1").style.display="none";
+    }
+    if (document.getElementById('coche2').checked) {
+        document.getElementById('image2').style.display="block";
+    } else {
+        document.getElementById("image2").style.display="none";
+    }
+    if (document.getElementById('coche3').checked) {
+        document.getElementById('image3').style.display="block";
+    } else {
+        document.getElementById("image3").style.display="none";
     }
 }
