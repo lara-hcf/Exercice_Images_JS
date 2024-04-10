b6fb25792ad8e3fa5ebdf42d38c6fb67831802c8
lara-hcf <lara.hocf@gmail.com>
2024-04-10 16:03:41 +0200

    test

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,7 @@
 function cocherImage() {
     if (document.getElementById('coche1').checked) {
-        document.getElementById('image1').style.visibility="visible";
-    }
-    if (document.getElementById('coche2').checked) {
-        document.getElementById('image2').style.visibility="visible";
-    }
-    if (document.getElementById('coche3').checked) {
-        document.getElementById('image3').style.visibility="visible";
+        document.getElementById('image1').style.display="none";
+    } else {
+        document.getElementById("image1").style.display="block";
     }
 }
