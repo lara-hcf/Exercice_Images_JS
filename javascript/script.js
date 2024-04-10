b7c82c1ead57b0a5cdd933799f8d105fff880224
lara-hcf <lara.hocf@gmail.com>
2024-04-10 14:53:05 +0200

    exercice_jss

---
+++ javascript/script.js
@@ -0,0 +1,11 @@
+function cocherImage() {
+    if (document.getElementById('coche1').checked) {
+        document.getElementById('image1').style.visibility="visible";
+    }
+    if (document.getElementById('coche2').checked) {
+        document.getElementById('image2').style.visibility="visible";
+    }
+    if (document.getElementById('coche3').checked) {
+        document.getElementById('image3').style.visibility="visible";
+    }
+}
